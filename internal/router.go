4f01d48298f630fc80921ee3c69a0b3a71438b44
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-08-11 07:45:33 +0500

    fix: server startup error round 3 — internal/router.go

--- internal/router.go
+++ internal/router.go
@@ -1,17 +1,5 @@
 package internal
 
-import (
-	"net/http"
-	"strconv"
-	"time"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/jmoiron/sqlx"
-	"github.com/rs/zerolog/log"
-)
-
-
 type BookCreate struct {
 	Title         string  `json:"title"`
 	Author        string  `json:"author"`
