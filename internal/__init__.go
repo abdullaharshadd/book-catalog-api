1752c8f354cd78e38700fd3c370d3d03cc57f098
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-09-03 03:16:36 +0500

    feat: migrate app/__init__.py → internal/__init__.go

---
+++ internal/__init__.go
@@ -0,0 +1,10 @@
+package internal
+
+// PackageVersion represents the version of the package.
+//nconst PackageVersion = "1.0.0"
+
+// PackageAuthor represents the author of the package.
+//nconst PackageAuthor = "Abdullah Arshad"
+
+// PackageEmail represents the email of the author of the package.
+//nconst PackageEmail = "abdullah.arshad.314@gmail.com"
